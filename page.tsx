ffc0dd5974def7ba689b686d3d9cf3b89a5430b8
Fuelrfun <caxasapiko@gmail.com>
2026-08-10 13:26:28 +0700

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,23 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import HowItWorks from "@/components/HowItWorks";
+import FeeFlow from "@/components/FeeFlow";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+
+      <Hero />
+
+      <Features />
+
+      <HowItWorks />
+
+      <FeeFlow />
+
+      <Footer />
+
+    </main>
+  );
+}
